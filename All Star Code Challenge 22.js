f2cd21454f9ba231623b7a1878df8c4d59a67004
michaelizaguirre501 <75349666+michaelizaguirre501@users.noreply.github.com>
2021-03-30 01:34:07 -0400

    Update All Star Code Challenge 22.js

--- All Star Code Challenge 22.js
+++ All Star Code Challenge 22.js
@@ -1,7 +1,5 @@
 //URL-- https://www.codewars.com/kata/5865cff66b5699883f0001aa/train/javascript
 
-//This Kata is intended as a small challenge for my students
-
 // All Star Code Challenge #22
 
 // Create a function hat takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds.
