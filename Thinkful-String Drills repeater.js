b31beaeed31d43e24ebb97719bef9c634c71d885
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-04-01 10:59:22 -0400

    added thinkful string drills repeater

---
+++ Thinkful-String Drills repeater.js
@@ -0,0 +1,23 @@
+//URL-- https://www.codewars.com/kata/585a1a227cb58d8d740001c3/train/javascript
+
+//Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
+
+// Example:
+// Repeater.repeat("a", 5)
+
+// should return
+// "aaaaa"
+
+
+//I will make an empty array and loop the given number of times each time pushing the given string to the array 
+//then return the joined array
+
+function repeater(string, n) {
+    let arr = []
+    for (let i = 0; i < n; i++) {
+        arr.push(string)
+    }
+    return arr.join('')
+
+
+}
