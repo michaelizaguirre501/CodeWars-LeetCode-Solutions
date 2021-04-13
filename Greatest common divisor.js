a44a6bc688e7214bf5fbb2fef9ad046833787866
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-04-13 13:21:37 -0400

    added gcp codewar

---
+++ Greatest common divisor.js
@@ -0,0 +1,15 @@
+//URL-- https://www.codewars.com/kata/5500d54c2ebe0a8e8a0003fd/train/javascript
+
+//Instructions
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+//Solution-- 
+// not gunna lie stack overflow. i was close but i didnt utilize the loop 
+
+
+function mygcd(x, y) {
+    while (y)[x, y] = [y, x % y];
+    return x;
+
+}
