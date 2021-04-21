6f9a23481dbe1b019bdee59376317e66c166c7a5
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-04-20 21:43:28 -0400

    fixed case for Uppercase letters

--- The Wide-Mouth frog!.js
+++ The Wide-Mouth frog!.js
@@ -6,8 +6,8 @@
 //Your goal in this kata is to create complete the mouth_size method this method take one argument animal which corresponds to the animal encountered by frog. If this one is an alligator (case insensitive) return small otherwise return wide.
 
 //SOLUTION--
-//Using a ternary I will check if the argument is alligator and return small if so and wide if not
-const mouthSize = (animal) => animal === 'alligator' ? 'small' : 'wide';
+//Using a ternary I will check if the argument when passed to lowercase is alligator and return small if so and wide if not
+const mouthSize = (animal) => animal.toLowerCase() === 'alligator' ? 'small' : 'wide';
 
 
 //TESTCASES--
