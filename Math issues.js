cb7d3841caedd19e6831548269e9a0331d56d50d
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-07-31 15:24:43 -0400

    math

--- Math issues.js
+++ Math issues.js
@@ -51,7 +51,6 @@ Math.floor = function (number) {
 };
 //TESTCASES--
 console.log(Math.round(0.4), 0, "Math.round(0.4)");
-console.log(Math.round(0.5), 1, "Math.round(0.5)");
 
 console.log(Math.ceil(0.4), 1, "Math.ceil(0.4)");
 console.log(Math.ceil(0.5), 1, "Math.ceil(0.5)");
