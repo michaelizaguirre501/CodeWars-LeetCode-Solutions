b033595dd9a7035410d078beaba8b071d85f6f10
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-08-09 14:34:48 -0400

    fizzbuzzington

---
+++ fizzbuzz.js
@@ -0,0 +1,25 @@
+//Fizz Buzz
+
+/* Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”. */
+
+const fizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0 && i % 3 === 0) {
+      console.log("Fizz Buzz");
+    } else if (i % 2 === 0) {
+      console.log("Fizz");
+    } else if (i % 3 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+
+  if (n < 0) {
+    console.log("Pick a number Greater than 0");
+  }
+};
+
+fizzBuzz(10);
+fizzBuzz(100);
+fizzBuzz(-10);
