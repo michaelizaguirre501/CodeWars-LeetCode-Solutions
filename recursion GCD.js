3dd3aa0627d0663f7febaee6a938fe7795199de7
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-09-04 15:48:52 -0400

    recursion

---
+++ recursion GCD.js
@@ -0,0 +1,15 @@
+//URL--https://www.codewars.com/kata/5500d54c2ebe0a8e8a0003fd/train/javascript
+
+//INSTRUCTIONS--
+// Find the greatest common divisor of two positive integers. The integers can be large, so you need to find a clever solution.
+// The inputs x and y are always greater or equal to 1, so the greatest common divisor will always be an integer that is also greater or equal to 1.
+
+//SOLUTION--
+function mygcd(x, y) {
+  return y == 0 ? x : mygcd(y, x % y);
+}
+
+//TESTCASES--
+console.log(mygcd(30, 12), 6);
+console.log(mygcd(8, 9), 1);
+console.log(mygcd(1, 1), 1);
