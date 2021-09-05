3fa2377a346b07c41e2a886e4e67613eb8feeebf
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-09-05 15:40:36 -0400

    recursion 1

---
+++ Recursion 1.js
@@ -0,0 +1,26 @@
+//URL-- https://www.codewars.com/kata/5694cb0ec554589633000036/train/javascript
+
+//INSTRUCTIONS--
+// Do you know recursion?
+// This is a kata series that you can only solve using recursion.
+// ##1 - Factorial
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example,
+// 5! = 5 * 4 * 3 * 2 * 1 = 120.
+// The value of 0! is 1.
+// #Your task
+// You have to create the function factorial that receives n and returns n!. You have to use recursion.
+
+//SOLUTION--
+const factorial = (n) => {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+
+//TESTCASES--
+console.log(factorial(0), 1);
+console.log(factorial(1), 1);
+console.log(factorial(2), 2);
+console.log(factorial(3), 6);
